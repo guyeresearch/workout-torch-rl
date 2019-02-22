94569982aebe360e03f6830ae00519f70d3cbfcb
Qiaonan Duan <113605074@qq.com>
2019-02-22 13:34:28 +0800

    run ddpg

--- mt_car_continuous/DDPG/run.py
+++ mt_car_continuous/DDPG/run.py
@@ -40,7 +40,10 @@ policy_optim = optim.Adam(policy.parameters(),lr=policy_lr)
 buffer = Buffer(batch_size,buffer_max)
 
 env = gym.make('MountainCarContinuous-v0')
-for k in range(epochs):
+for k in range(epochs)[20:]:
+    print('epoch {}'.format(k))
+#    if k == rand_explore_epochs:
+#        qn.dump(buffer,'buffer_rand.pkl')
     added = 0
     while added < epoch_eps_size:
         eps = []
@@ -50,17 +53,20 @@ for k in range(epochs):
             if k < rand_explore_epochs:
                 a = env.action_space.sample()
             else:
-                a = [policy(obs).data.tolist()[0] + 
+                obs_tensor = torch.tensor(obs,dtype=torch.float)
+                a = [policy(obs_tensor).data.tolist()[0] + 
                     np.random.normal(scale=0.5)]
             obs_new, r, done, info = env.step(a)
             eps.append([obs,a,r,obs_new, done])
             obs = obs_new
         if len(eps) < 999:
+            print('added {} eps, current size {}'.format(added, len(eps)))
             added += 1
             buffer.add_many(eps)
     
 
-    for _ in range(update_num):
+    for j in range(update_num):
+        print('perform {} update'.format(j))
         batch = buffer.sample()
         obs, a, r, obs_new, done = [torch.tensor(x,dtype=torch.float) 
             for x in zip(*batch)]
@@ -86,4 +92,21 @@ for k in range(epochs):
         for w, w_target in zip(policy.parameters(),policy_target.parameters()):
             w_target = rho*w_target + (1-rho)*w
         
-        
+
+
+#%% test
+env = gym.make('MountainCarContinuous-v0')
+for i_episode in range(5):
+    obs = env.reset()
+    for t in range(2000):
+        if (t+1)%100 == 0:
+            print(t+1)
+        env.render()
+        obs_tensor = torch.from_numpy(obs.astype('float32'))
+        a = policy(obs_tensor)
+#        a = [a.data.tolist()[0] + 
+#                    np.random.normal(scale=0.5)]
+        a = a.data.tolist()
+        obs_new, r, done, info = env.step(a)
+        obs = obs_new
+
